e19375d9d8ef33a08039d98f38d5892c93a8c04d
juampa <jpasosa@gmail.com>
2019-02-11 11:18:07 -0300

    agrego lineas para poder debuguear bien

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,13 @@
 // Version
 define('VERSION', '3.0.2.0');
 
+
+// enviroment developers
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 // Configuration
 if (is_file('config.php')) {
 	require_once('config.php');
